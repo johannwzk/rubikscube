56d1837b06dfb4d633d6b3f6d1ce5ac1f7c6aeda
PlanetBuilder <planetbuilder.accs@gmail.com>
2022-05-31 21:16:56 +0200

    added setPosition methods in CubePosition

--- src/CubePosition.java
+++ src/CubePosition.java
@@ -14,26 +14,48 @@ public class CubePosition {
         this.ro = ro;
     }
 
-    public int toInt() {
-        int wyfactor;
-        int rofactor;
-        int gbfactor;
+    public void setPosition(int wy, int ro, int gb) {
+        if (wy < -1 || wy > 1) throw new CorruptedCubePositionException("Cube position 'wy' corrupted: " + wy + " (should be either -1, 0 or 1)");
+        else if (ro < -1 || ro > 1) throw new CorruptedCubePositionException("Cube position 'ro' corrupted: " + ro + " (should be either -1, 0 or 1)");
+        else if (gb < -1 || gb > 1) throw new CorruptedCubePositionException("Cube position 'gb' corrupted: " + gb + " (should be either -1, 0 or 1)");
+        this.wy = wy;
+        this.gb = gb;
+        this.ro = ro;
+    }
 
-        if (wy == 1) wyfactor = ColorFactor.WHITE;
-        else if (wy == -1) wyfactor = ColorFactor.YELLOW;
-        else if (wy == 0) wyfactor = ColorFactor.NONE;
+    public void setPosition(int position) {
+        if (position % ColorFactor.WHITE == 0 && (position & ColorFactor.YELLOW) == 0) throw new IllegalArgumentException("Position cannot contain factors for white and yellow");
+        if (position % ColorFactor.RED == 0 && (position & ColorFactor.ORANGE) == 0) throw new IllegalArgumentException("Position cannot contain factors for white and yellow");
+        if (position % ColorFactor.GREEN == 0 && (position & ColorFactor.BLUE) == 0) throw new IllegalArgumentException("Position cannot contain factors for white and yellow");
+
+        if (position % ColorFactor.WHITE == 0) this.wy = 1;
+        else if (position % ColorFactor.YELLOW == 0) this.wy = -1;
+        if (position % ColorFactor.RED == 0) this.ro = 1;
+        else if (position % ColorFactor.ORANGE == 0) this.ro = -1;
+        if (position % ColorFactor.GREEN == 0) this.gb = 1;
+        else if (position % ColorFactor.BLUE == 0) this.gb = -1;
+    }
+
+    public int toInt() {
+        int wyFactor;
+        int roFactor;
+        int gbFactor;
+
+        if (this.wy == 1) wyFactor = ColorFactor.WHITE;
+        else if (this.wy == -1) wyFactor = ColorFactor.YELLOW;
+        else if (this.wy == 0) wyFactor = ColorFactor.NONE;
         else throw new CorruptedCubePositionException("Cube position 'wy' corrupted: " + wy + " (should be either -1, 0 or 1)");
 
-        if (ro == 1) rofactor = ColorFactor.RED;
-        else if (ro == -1) rofactor = ColorFactor.ORANGE;
-        else if (ro == 0) rofactor = ColorFactor.NONE;
+        if (this.ro == 1) roFactor = ColorFactor.RED;
+        else if (this.ro == -1) roFactor = ColorFactor.ORANGE;
+        else if (this.ro == 0) roFactor = ColorFactor.NONE;
         else throw new CorruptedCubePositionException("Cube position 'ro' corrupted: " + ro + " (should be either -1, 0 or 1)");
 
-        if (gb == 1) gbfactor = ColorFactor.GREEN;
-        else if (gb == -1) gbfactor = ColorFactor.BLUE;
-        else if (gb == 0) gbfactor = ColorFactor.NONE;
+        if (this.gb == 1) gbFactor = ColorFactor.GREEN;
+        else if (this.gb == -1) gbFactor = ColorFactor.BLUE;
+        else if (this.gb == 0) gbFactor = ColorFactor.NONE;
         else throw new CorruptedCubePositionException("Cube position 'gb' corrupted: " + gb + " (should be either -1, 0 or 1)");
 
-        return wyfactor*rofactor*gbfactor;
+        return wyFactor*roFactor*gbFactor;
     }
 }
