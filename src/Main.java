16ebb25dcda714e60bd526f3264820c5999917a1
PlanetBuilder <planetbuilder.accs@gmail.com>
2022-05-31 22:06:06 +0200

    Cleaned up main method

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,17 @@ import GLOOP.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private static final String axises = "wyrogb";
-
-    // Wuerfel[x][y][z]
-    static GLWuerfel[][][] cubes = new GLWuerfel[3][3][3];
-
     public static void main(String[] args) {
-        GLKamera kamera = new GLKamera(1280, 720);
-        GLLicht licht = new GLLicht();
-        kamera.setzePosition(500, 300, -200);
+        GLKamera camera = new GLKamera(1280, 720); camera.setzePosition(0, 200, 500);
+        GLLicht light = new GLLicht(5000, 10000, 2500);
+        Cube cube = new Cube(200, 4);
 
-        Cube cube = new Cube(310, 5);
+        while (true){
+            camera.rotiere(0.01, 0, 1, 0, 0, 0, 0);
+            light.rotiere(0.01, 0, 1, 0, 0 , 0, 0);
 
-//        for (int z = 0; z < 3; z++) {
-//            for (int y = 0; y < 3; y++) {
-//                for (int x = 0; x < 3; x++) {
-//                    cubes[x][y][z] = new GLWuerfel((x-1)*100, (y-1)*100, (z-1)*100, 95);
-//                }
-//            }
-//        }
-//
-//        while (true){
-//            for (int z = 0; z < 3; z++) {
-//                for (int y = 0; y < 3; y++) {
-//                    cubes[0][y][z].rotiere(3, 1, 0, 0,(-1)*100, 0, 0);
-//                }
-//            }
-//
-//            try {
-//                TimeUnit.MILLISECONDS.sleep(50);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
+            try { TimeUnit.NANOSECONDS.sleep(1); }
+            catch (InterruptedException e) { throw new RuntimeException(e); }
+        }
     }
 }
