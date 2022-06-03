68b83dfbb8c5a278fc7e6fae6faf9e0eb2752da3
PlanetBuilder <planetbuilder.accs@gmail.com>
2022-06-03 19:50:28 +0200

    Added Testtextur.png, changed CubePart from GLWürfel to GLTafel

--- src/CubePart.java
+++ src/CubePart.java
@@ -1,14 +1,40 @@
+import GLOOP.GLTafel;
 import GLOOP.GLTextur;
 import GLOOP.GLVektor;
 import GLOOP.GLWuerfel;
 
-public class CubePart extends GLWuerfel{
+public class CubePart {
     public final CubePosition originalPosition;
     public CubePosition currentPosition;
 
+    private final GLTafel[] cubeSides;
+
     public CubePart(CubePosition cubePosition, double partSize, double spacing) {
         //TODO: automated texture
-        super(cubePosition.x()*(partSize+spacing), cubePosition.y()*(partSize+spacing), cubePosition.z()*(partSize+spacing), partSize);
+
+        //TODO: QUADER
+        cubeSides = new GLTafel[6];
+        cubeSides[0] = new GLTafel(cubePosition.x()*(partSize+spacing), cubePosition.y()*(partSize+spacing)-partSize/2, cubePosition.z()*(partSize+spacing), partSize, partSize);
+        cubeSides[0].setzeAutodrehung(false);
+        cubeSides[0].rotiere(90, 1, 0, 0, cubeSides[0].gibX(), cubeSides[0].gibY(), cubeSides[0].gibZ());
+        cubeSides[0].setzeTextur("textures/Testtextur.png");
+        cubeSides[1] = new GLTafel(cubePosition.x()*(partSize+spacing), cubePosition.y()*(partSize+spacing)+partSize/2, cubePosition.z()*(partSize+spacing), partSize, partSize);
+        cubeSides[1].setzeAutodrehung(false);
+        cubeSides[1].rotiere(90, 1, 0, 0, cubeSides[1].gibX(), cubeSides[1].gibY(), cubeSides[0].gibZ());
+        cubeSides[2] = new GLTafel(cubePosition.x()*(partSize+spacing)-partSize/2, cubePosition.y()*(partSize+spacing), cubePosition.z()*(partSize+spacing), partSize, partSize);
+        cubeSides[2].setzeAutodrehung(false);
+        cubeSides[2].rotiere(90, 0, 1, 0, cubeSides[2].gibX(), cubeSides[2].gibY(), cubeSides[0].gibZ());
+        cubeSides[3] = new GLTafel(cubePosition.x()*(partSize+spacing)+partSize/2, cubePosition.y()*(partSize+spacing), cubePosition.z()*(partSize+spacing), partSize, partSize);
+        cubeSides[3].setzeAutodrehung(false);
+        cubeSides[3].rotiere(90, 0, 1, 0, cubeSides[3].gibX(), cubeSides[3].gibY(), cubeSides[0].gibZ());
+        cubeSides[4] = new GLTafel(cubePosition.x()*(partSize+spacing), cubePosition.y()*(partSize+spacing), cubePosition.z()*(partSize+spacing)-partSize/2, partSize, partSize);
+        cubeSides[4].setzeAutodrehung(false);
+        cubeSides[4].rotiere(0, 0, 1, 0, cubeSides[4].gibX(), cubeSides[4].gibY(), cubeSides[0].gibZ());
+        cubeSides[5] = new GLTafel(cubePosition.x()*(partSize+spacing), cubePosition.y()*(partSize+spacing), cubePosition.z()*(partSize+spacing)+partSize/2, partSize, partSize);
+        cubeSides[5].setzeAutodrehung(false);
+        cubeSides[5].rotiere(0, 0, 1, 0, cubeSides[5].gibX(), cubeSides[5].gibY(), cubeSides[0].gibZ());
+
+
         this.originalPosition = cubePosition;
         this.currentPosition = cubePosition;
     }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         GLKamera camera = new GLKamera(1280, 720); camera.setzePosition(0, 200, 500);
         GLLicht light = new GLLicht(5000, 10000, 2500);
-        Cube cube = new Cube(200, 4);
+//        Cube cube = new Cube(200, 4);
+//        cube.cubeParts[0].setzeTextur("/home/planetbuilder/orgel.png");
 
+        CubePart cubePart = new CubePart(new CubePosition(-1, 0, 1), 100, 5);
         while (true){
             camera.rotiere(0.01, 0, 1, 0, 0, 0, 0);
             light.rotiere(0.01, 0, 1, 0, 0 , 0, 0);
